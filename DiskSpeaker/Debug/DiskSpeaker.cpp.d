59efb75d03457d6cd2f7346601676272d7c1dddb
Saleh <Saleh.nouri@gmail.com>
2017-10-04 20:05:09 +0330

    Touch Start

--- DiskSpeaker/Debug/DiskSpeaker.cpp.d
+++ DiskSpeaker/Debug/DiskSpeaker.cpp.d
@@ -1,21 +1,23 @@
-C:\Users\Saleh\AppData\Local\Temp\VMBuilds\DiskSpeaker\lilypad_atmega328\Debug\DiskSpeaker.cpp.o: \
- C:\Users\Saleh\AppData\Local\Temp\VMBuilds\DiskSpeaker\lilypad_atmega328\Debug\DiskSpeaker.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+C:\Users\Saleh\AppData\Local\Temp\VMBuilds\DiskSpeaker\pro_16MHzatmega328\Debug\DiskSpeaker.cpp.o: \
+ C:\Users\Saleh\AppData\Local\Temp\VMBuilds\DiskSpeaker\pro_16MHzatmega328\Debug\DiskSpeaker.cpp \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/binary.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/WCharacter.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/WString.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/HardwareSerial.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Stream.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Print.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Printable.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/USBAPI.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Arduino.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs/pins_arduino.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\variants\eightanaloginputs/../standard/pins_arduino.h \
  C:\Users\Saleh\Documents\Arduino\libraries\PT2314/PT2314.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\EEPROM\src/EEPROM.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\libraries\Wire\src/Wire.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\cores\arduino/Stream.h \
+ C:\Users\Saleh\AppData\Local\arduino15\packages\arduino\hardware\avr\1.6.20\libraries\EEPROM\src/EEPROM.h \
  C:\Users\Saleh\Documents\Arduino\libraries\Adafruit_MPR121/Adafruit_MPR121.h \
  C:\Users\Saleh\Documents\Arduino\libraries\IRremote/IRremote.h \
  C:\Users\Saleh\Documents\Arduino\libraries\IRremote/IRremoteInt.h \
+ C:\Users\Saleh\Documents\Arduino\libraries\IRremote/boarddefs.h \
  C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\sek0yots.tnz\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
--- DiskSpeaker/DiskSpeaker.ino
+++ DiskSpeaker/DiskSpeaker.ino
@@ -11,6 +11,7 @@ Author:	Saleh
 #include <IRremote.h>
 #include <Wire.h>
 
+#include "Adafruit_MPR121.h"
 // Uncomment this line to enable output to the serial monitor (at 9600 baud).
 #define SERIAL_MONITOR
 
@@ -24,6 +25,15 @@ Author:	Saleh
 // EEPROM Addresses
 #define SETTINGS_ADDRESS          64
 #define SETTINGS_ID_STRING        "AET 1.0"
+
+// You can have up to 4 on one i2c bus but one is enough for testing!
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
 
 typedef struct SettingsType {
 	char        id[8];
@@ -174,6 +184,16 @@ void setup() {
 
 	//make sure that LEDs are off on Power up.
 	Clear_LED();
+
+	Serial.println("Adafruit MPR121 Capacitive Touch sensor test");
+
+	// Default address is 0x5A, if tied to 3.3V its 0x5B
+	// If tied to SDA its 0x5C and if SCL then 0x5D
+	//if (!cap.begin(0x5B)) {
+	//	Serial.println("MPR121 not found, check wiring?");
+	//	//while (1);
+	//}
+	//Serial.println("MPR121 found!");
 
 }
 void loop() {
